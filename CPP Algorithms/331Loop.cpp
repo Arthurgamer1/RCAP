13a17724fa8d4ad924e393243d380c0f8455ade5
Brian <arthurgamer198@gmail.com>
2025-04-16 13:50:48 -0800

    Adding optimized example to loop.cpp

--- CPP Algorithms/331Loop.cpp
+++ CPP Algorithms/331Loop.cpp
@@ -11,29 +11,30 @@ int main()
     auto start_2 = high_resolution_clock::now();
 
     //Start measuring CPU time. CPU time is the time it takes for the cpu to finish the execution of the program
-    auto cpu_start = high_resolution_clock::now();
 
-    //Loop Sum
-    int sum = 0;
-    int n = 1000000;
-    for (int i = 0; i < n; i++) {
-        sum += i;
-    }
+    //Unoptimized lop sum
+    int n = 100000;
+    //int sum = 0;
+    //for (int i = 0; i <= n; i++) {
+      //  sum += i;
+    //}
+
+
+    //Optimized Loop sum
+    long long sum = (static_cast<long long>(n) * (n + 1)) / 2;
+
 
     //End CPU time 
-    auto cpu_stop = high_resolution_clock::now();
 
     //End clock time 
     auto stop_2 = high_resolution_clock::now();
 
     //CPU time in microseconds
-    auto cpu_duration = duration_cast<microseconds>(cpu_stop - cpu_start);
 
     //Time for clock 
     auto real_duration = duration_cast<microseconds>(stop_2 - start_2);
 
     //Print results
-    cout << "CPU Time: " << cpu_duration.count() << " microseconds" << endl;
     cout << "Real Time: " << real_duration.count() << " microseconds" << endl;
     cout << "Sum: " << sum << endl;
 
